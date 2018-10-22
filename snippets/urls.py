fd966f0b33e905c61cf9f66c7cbeb39f90aa98b3
Nadien-Rashad <nadienrashad@gmail.com>
2018-10-22 21:32:51 +0200

    3tutorials Rest-framework

---
+++ snippets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+    # def post(se;f, request, format=None):
+    #     serializer = SnippetSerializer(snippets, many=True)
